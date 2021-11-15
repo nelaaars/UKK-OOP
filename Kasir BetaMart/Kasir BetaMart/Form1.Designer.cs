452d32af85c1bc0421286bc3025497bb7c95b25a
nelaaars <54801177+nelaaars@users.noreply.github.com>
2021-11-16 06:10:18 +0700

    kasir-betamart

---
+++ Kasir BetaMart/Kasir BetaMart/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace Kasir_BetaMart
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtKode = new System.Windows.Forms.TextBox();
+            this.txtHarga = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.btnSImpan = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnBrowse.Location = new System.Drawing.Point(96, 255);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(56, 310);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 25;
+            this.dataGridView.Size = new System.Drawing.Size(466, 158);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(200, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "KASIR BETAMART";
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel.Controls.Add(this.label1);
+            this.panel.Location = new System.Drawing.Point(-4, -1);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(588, 62);
+            this.panel.TabIndex = 4;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // txtID
+            // 
+            this.txtID.ForeColor = System.Drawing.Color.Black;
+            this.txtID.Location = new System.Drawing.Point(331, 88);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(191, 23);
+            this.txtID.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kode Barang";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(241, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Nama Barang";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(241, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Harga";
+            // 
+            // txtKode
+            // 
+            this.txtKode.Location = new System.Drawing.Point(331, 131);
+            this.txtKode.Name = "txtKode";
+            this.txtKode.Size = new System.Drawing.Size(191, 23);
+            this.txtKode.TabIndex = 11;
+            // 
+            // txtHarga
+            // 
+            this.txtHarga.Location = new System.Drawing.Point(331, 212);
+            this.txtHarga.Name = "txtHarga";
+            this.txtHarga.Size = new System.Drawing.Size(191, 23);
+            this.txtHarga.TabIndex = 12;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(331, 173);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(191, 23);
+            this.txtNama.TabIndex = 13;
+            // 
+            // btnSImpan
+            // 
+            this.btnSImpan.Location = new System.Drawing.Point(266, 488);
+            this.btnSImpan.Name = "btnSImpan";
+            this.btnSImpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSImpan.TabIndex = 15;
+            this.btnSImpan.Text = "Simpan";
+            this.btnSImpan.UseVisualStyleBackColor = true;
+            this.btnSImpan.Click += new System.EventHandler(this.btnSImpan_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(357, 488);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 16;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(448, 488);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 17;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(56, 88);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(155, 156);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(579, 532);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSImpan);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtHarga);
+            this.Controls.Add(this.txtKode);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtKode;
+        private System.Windows.Forms.TextBox txtHarga;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Button btnSImpan;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Kasir BetaMart/Kasir BetaMart/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kasir_BetaMart
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\kasirbetamart.accdb");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSImpan_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = new OleDbCommand("insert into kasirbetamart (ID,Kode_Barang,Nama_Barang ,Harga) values('"+ txtID.Text +"','" + txtKode.Text + "','" + txtNama.Text + "','" + txtHarga.Text + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Data Berhasil Disimpan.");
+
+            clearText();
+            fillgrid();
+        }
+        void fillgrid()
+        {
+            con.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from kasirbetamart order by ID", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView.DataSource = dt;
+            con.Close();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = new OleDbCommand("update kasirbetamart set ID  ='" + txtID.Text + "',Kode_Barang ='" + txtKode.Text + "', Nama_Barang = '" + txtNama.Text + "',Harga = '" + txtHarga.Text + "' where ID = '" + txtID.Text + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Data Berhasil Diubah.");
+
+            clearText();
+            fillgrid();
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = new OleDbCommand("delete from kasirbetamart where ID ='" + txtID.Text + "' ", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Data Berhasil Dihapus.");
+
+            clearText();
+            fillgrid();
+        }
+
+        void clearText()
+        {
+            txtID.Text = "";
+            txtKode.Text = "";
+            txtNama.Text = "";
+            txtHarga.Text = "";
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string filePart = openFileDialog1.FileName;
+            pictureBox.Image = Image.FromFile(filePart);
+        }
+
+        private void txtCari_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        void TampilBarang()
+        {
+
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+        }
+
+        private void btnCari_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
